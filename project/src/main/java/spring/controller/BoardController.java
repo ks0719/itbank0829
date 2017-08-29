22e122f7e3f1f7d829f0538797de20e98f65e5ac
Nelo <Nelo@Neloangello>
2017-08-29 20:49:54 +0900

    board

--- project/src/main/java/spring/controller/BoardController.java
+++ project/src/main/java/spring/controller/BoardController.java
@@ -1,8 +1,32 @@
 package spring.controller;
 
-public class BoardController {
+import org.springframework.web.bind.annotation.RequestMapping;
 
- public void test() {
-	 
- }
+public class BoardController {
+	
+	@RequestMapping("/free")
+	public String free() {
+		return "board/free";
+	}
+	
+	@RequestMapping("/info")
+	public String info() {
+		return "board/info";
+	}
+	
+	@RequestMapping("/qna")
+	public String qna() {
+		return "board/qna";
+	}
+	
+	@RequestMapping("require")
+	public String require() {
+		return "board/require";
+	}
+	
+	@RequestMapping("store")
+	public String store() {
+		return "board/store";
+	}
+	
 }
