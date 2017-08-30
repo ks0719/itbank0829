1212428edf043af4c38b9b902a258a110bbb5001
user <user@pc>
2017-08-30 21:44:52 +0900

    고객센터 1:1문의

--- project/src/main/java/spring/controller/CustomerController.java
+++ project/src/main/java/spring/controller/CustomerController.java
@@ -1,14 +1,44 @@
 package spring.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import spring.db.B2CDao;
+import spring.db.B2CDto;
 
 @Controller
 public class CustomerController {
+	@Autowired
+	private B2CDao dao;
+	private B2CDto dto;
 	@RequestMapping("/consumer/b2c")
-	public String inquire() {
+	public String b2c() {
 		return "consumer/b2c";
 	}
+	@RequestMapping(value="/consumer/b2c",method=RequestMethod.POST)
+	public String b2cpost(HttpServletRequest request, Model model) {
+	dto=new B2CDto(request);
+	dao.insert(dto);
+	List<B2CDto> list;
+	list=dao.list("aaaa");
+	model.addAttribute("list", list);
+	return "redirect:/consumer/b2clist";
+	}
+	@RequestMapping(value="/consumer/b2clist")
+	public String b2clist(Model model) {
+		List<B2CDto> list;
+		list=dao.list("aaaa");
+		model.addAttribute("list", list);
+		return "consumer/b2clist";
+	}
+	
 	
 	@RequestMapping("/consumer/basic")
 	public String QnA() {
---
+++ project/src/main/java/spring/db/B2CDto.java
@@ -0,0 +1,137 @@
+package spring.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class B2CDto {
+private int rn;
+private int no;
+private String id;
+private String type;
+private String title;
+private String detail;
+private String filename;
+private String realname;
+private String filetype;
+private long filesize;
+private String reg;
+private String state;
+
+public B2CDto() {
+	super();
+}
+public B2CDto(HttpServletRequest request) {
+	
+	setId(request.getParameter("id"));
+	setType(request.getParameter("type"));
+	setTitle(request.getParameter("title"));
+	setDetail(request.getParameter("detail"));
+//	setFilename(request.getParameter("filename"));
+//	setFiletype(request.getParameter("filetype"));
+//	setFilesize(Long.parseLong(request.getParameter("filesize")));
+	setFilename("이름");
+	setFiletype("타입");
+	setFilesize(1245);
+	
+}
+public B2CDto(ResultSet rs) throws SQLException {
+	setRn(rs.getInt("rn"));
+	setNo(rs.getInt("no"));
+	setId(rs.getString("id"));
+	setType(rs.getString("type"));
+	setTitle(rs.getString("title"));
+	setDetail(rs.getString("title"));
+//	setFilename(rs.getString("filename"));
+//	setRealname(rs.getString("realname"));
+//	setFiletype(rs.getString("filetype"));
+//	setFilesize(rs.getLong("filesize"));
+	setFilename("이름");
+	setRealname("진짜이름");
+	setFiletype("사진");
+	setFilesize(1245);
+	setReg(rs.getString("reg"));
+	setState(rs.getString("state"));
+}
+public void setRn(int rn) {
+	this.rn = rn;
+}
+public int getRn() {
+	return rn;
+}
+public void setNo(int no) {
+	this.no = no;
+}
+public int getNo() {
+	return no;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getDetail() {
+	return detail;
+}
+public void setDetail(String detail) {
+	this.detail = detail;
+}
+public String getFilename() {
+	return filename;
+}
+public void setFilename(String filename) {
+	this.filename = filename;
+}
+public String getRealname() {
+	return realname;
+}
+public void setRealname(String realname) {
+	this.realname = realname;
+}
+public String getFiletype() {
+	return filetype;
+}
+public void setFiletype(String filetype) {
+	this.filetype = filetype;
+}
+public long getFilesize() {
+	return filesize;
+}
+public void setFilesize(long filesize) {
+	this.filesize = filesize;
+}
+public String getReg() {
+	return reg;
+}
+public void setReg(String reg) {
+	this.reg = reg;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+@Override
+public String toString() {
+	return "B2CDto [" + ", id=" + id + ", type=" + type + ", title=" + title + ", detail=" + detail
+			+ ", filename=" + filename + ", realname=" + realname + ", filetype=" + filetype + ", filesize=" + filesize
+			+ ", reg=" + reg + ", state=" + state + "]";
+}
+
+
+}
