5bcf6532583da06940e311b5d1f15e1aefc3ae91
ITBANK <ITBANK@DESKTOP-U8MQ6UF>
2017-09-04 20:22:18 +0900

    a

--- project/src/main/java/spring/db/mail/MailDao.java
+++ project/src/main/java/spring/db/mail/MailDao.java
@@ -21,25 +21,25 @@ public class MailDao {
 	
 	public List<Mail> list(String mail_receiver, String box){
 		
-		System.out.println(box);
 		
 		String sql="select * from mail";
 		switch(box) {
 			//무관
 		case "protect":
 		case "garbage":
-			sql+=" where mail_position=? or mail_receiver=? and mail_position=? order by mail_reg desc";
+			sql+=" where mail_position=? or mail_receiver=? and mail_position=? order by mail_reg desc, no desc";
 			return jdbcTemplate.query(sql, new Object[] {mail_receiver,mail_receiver,box},mapper);
-			//받는 사람이 나일때 
-		case "index":
-			sql+=" where mail_receiver=? and mail_position=? order by mail_reg desc";
-			break;
+			
 			//팀원이랑 상의
 		case "spam":
 			//보낸 사람이 나일때
 		case "sent":
-			sql+=" where mail_writer=? and mail_position=? order by mail_reg desc";
+			sql+=" where mail_writer=? and mail_position=? order by mail_reg desc, no desc";
 			break;
+			//받는 사람이 나일때  or box가 없을때
+		default:
+			sql+=" where mail_receiver=? and mail_position='index' order by mail_reg desc, no desc";
+			return jdbcTemplate.query(sql, new Object[] {mail_receiver},mapper);
 		}
 //		"select * from mail where mail_writer=? order by mail_reg desc"
 		return jdbcTemplate.query(sql, new Object[] {mail_receiver,box},mapper);
