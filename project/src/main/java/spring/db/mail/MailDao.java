72aac8ccd1454be0ebcf3ff1f3622f240a8e6fdb
Nelo <Nelo@Neloangello>
2017-09-04 18:11:05 +0900

    1

--- project/src/main/java/spring/db/mail/MailDao.java
+++ project/src/main/java/spring/db/mail/MailDao.java
@@ -18,7 +18,9 @@ public class MailDao {
 		return new Mail(rs);
 	};
 	
-	public List<Mail> list(String mail_writer, String box){
+	public List<Mail> list(String mail_receiver, String box){
+		
+		System.out.println(box);
 		
 		String sql="select * from mail";
 		switch(box) {
@@ -26,21 +28,19 @@ public class MailDao {
 		case "protect":
 		case "garbage":
 			sql+=" where mail_position=? or mail_receiver=? and mail_position=? order by mail_reg desc";
-			return jdbcTemplate.query(sql, new Object[] {mail_writer,mail_writer,box},mapper);
+			return jdbcTemplate.query(sql, new Object[] {mail_receiver,mail_receiver,box},mapper);
 			//받는 사람이 나일때 
 		case "index":
-		case "spam":
 			sql+=" where mail_receiver=? and mail_position=? order by mail_reg desc";
 			break;
-			
+			//팀원이랑 상의
+		case "spam":
 			//보낸 사람이 나일때
 		case "sent":
 			sql+=" where mail_writer=? and mail_position=? order by mail_reg desc";
 			break;
 		}
 //		"select * from mail where mail_writer=? order by mail_reg desc"
-		return jdbcTemplate.query(sql, new Object[] {mail_writer,box},mapper);
-		
-		
+		return jdbcTemplate.query(sql, new Object[] {mail_receiver,box},mapper);
 	}
 }
