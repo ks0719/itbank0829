ed6451b85be874303c6e6869350fba0bb0444e6a
IT202-03 <IT202-03@DESKTOP-AC1VQE3>
2017-09-04 21:39:22 +0900

    게시판 레이아웃

--- project/src/main/java/spring/controller/BoardController.java
+++ project/src/main/java/spring/controller/BoardController.java
@@ -1,34 +1,72 @@
 package spring.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import spring.db.board.BoardDao;
+import spring.db.lecture.LectureInfo;
+
 @Controller
+@RequestMapping("/board")
 public class BoardController {
+	private Logger log = LoggerFactory.getLogger(getClass());
 	
-	@RequestMapping("/board/free")
-	public String free() {
-		return "board/free";
-	}
+	@Autowired
+	private BoardDao boardDao;
 	
-	@RequestMapping("/board/info")
-	public String info() {
-		return "board/info";
-	}
-	
-	@RequestMapping("/board/qna")
-	public String qna() {
-		return "board/qna";
-	}
-	
-	@RequestMapping("/board/require")
-	public String require() {
-		return "board/require";
-	}
-	
-	@RequestMapping("/board/store")
-	public String store() {
-		return "board/store";
+	@RequestMapping("/{path}")
+	public String board(@PathVariable String path, HttpServletRequest request, Model m) {	
+		String type = request.getParameter("type");
+		String key = request.getParameter("key");
+		log.debug(path);
+		
+		int pageNo;
+		try {
+			pageNo = Integer.parseInt("page");
+		} catch(Exception e) {
+			pageNo = 1;
+		}
+		if (pageNo <= 0 ) pageNo = 1;
+
+		int listCount = boardDao.count(type, key);
+		log.debug(String.valueOf(listCount));
+		
+		int boardSize = 10;
+		int start = boardSize * pageNo - 9;
+		int end = start + boardSize -1;
+		if (end > listCount) end = listCount;
+		
+		List<LectureInfo> list = boardDao.list(type, key, start, end);
+		
+		int blockSize = 10;
+		int blockTotal = (listCount + boardSize - 1) / boardSize;
+		int startBlock = (pageNo - 1) / blockSize * blockSize + 1;
+		int endBlock = startBlock + blockSize - 1;
+		if (endBlock > blockTotal) endBlock = blockTotal;
+		
+		String url = path + "?";
+		if (type != null && key != null) {
+			url += "type=" + type + "&key=" + key + "&";
+			m.addAttribute("type", type);
+			m.addAttribute("key", key);
+		}
+		
+		m.addAttribute("list", list);
+		m.addAttribute("page", pageNo);
+		m.addAttribute("startBlock", startBlock);
+		m.addAttribute("endBlock", endBlock);
+		m.addAttribute("url", url);
+		return "board/" + path;
 	}
 	
 }
